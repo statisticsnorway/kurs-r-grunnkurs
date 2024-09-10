627b028a705ff3fdc0f4296cfbb6c320ce737c03
rdn <rdn@ssb.no>
2024-09-10 11:20:52 +0200

    Legger til befolkning_per_kommune.xlsx

---
+++ løsninger/Oppgaver 1 - løsning.R
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+# # Oppgaver 1
+
+# ## Åpne RStudio. Hva er konsollen og hva er miljøvinduet? Bruk  konsollen som kalkulator og gjør noen regnestykker. 
+
+1+1
+
+# ## I konsollen: definer objektet `mitt_tall` hvor du lagrer yndlingstallet ditt. Hva skjer i miljøvinduet når du gjør dette? 
+
+mitt_tall <- 1
+
+# ## Skriv `mitt_tall` i konsollen. Hva skjer?
+
+mitt_tall
+
+# ## Definer et annet objekt (finn på navnet selv) og gjør noen regnestykker med de to objektene du nå har definert.
+
+# +
+nytt_tall <- 2
+
+mitt_tall+nytt_tall
+# -
+
+# ## Definer et tredje objekt som summen av de to første objektene, 
+
+sum_tall <- mitt_tall+nytt_tall
+
+# ## Trykk på feiekosten i miljøvinduet. Hva skjer? 
+
+
+
+# ## Skriv `mitt_tall` i konsollen nå. Hva skjer?  
+
+mitt_tall
+
+# ## Opprett et nytt R skript fra nedtrekksmenyen. 
+
+
+
+# ## I skriptet definerer du de samme objektene som i oppgavene 2-4 (`mitt_tall` + egendefinert objekt). Kjør skriptet. Hva skjer? 
+
+
+
+# ## Lagre skriptet som `my_first_Rskript.R` eller noe lignende. 
+
+
+
+# ## Lukk skriptet, trykk på feiekosten i miljøvinduet. Kall på de definerte objektene i konsolen. Hva skjer?  
+
+
+
+# ## Åpne skriptet du nettopp lagde og kjør det på nytt.
+
+
+
+# ## Definer et objekt med fire desimaler.
+
+tall_desimaler <- 1.2345
+
+# ## Bruk funksjonen `round()` til å runde av dette objektet til ingen, én og to desimaler
+
+round(tall_desimaler, digits = 0)
+round(tall_desimaler, digits = 1)
+round(tall_desimaler, digits = 2)
+
+# ## Hva skjer hvis du skriver og kjører `3 > 2` i scriptet? Hva med `3 == 2`? Og `3 < 2`?  Hvilken type objekt har disse? (`class()`)
+
+# +
+3 > 2
+3 == 2
+3 < 2
+
+class(3 > 2)
+# -
+
+# ## Hva skjer hvis du skriver `3 = 2`?
+
+# +
+# 3 = 2
+# -
+
+# ## Lagre et objekt som er en vektor med tre tall. 
+
+vektor_tall <- c(1, 2, 3)
+
+# ## Hent det tredje elementet i vektorene. 
+
+vektor_tall[3]
+
+# ## Finn lengden på vektoren ved å bruke funksjonen `length()`
+
+length(vektor_tall)
+
+# ## Hva skjer hvis du anvender funksjonen `sum()`  på vektoren? 
+
+sum(vektor_tall)
+
+# ## Opprett en annen vektor, også denne med tre tall. 
+
+vektor_tall_2 <- c(4, 5, 6)
+
+# ## Gjør noen regneoperasjoner med vektorene. Hva skjer? 
+
+# +
+vektor_tall_2+1
+vektor_tall_2*2
+
+sum(vektor_tall_2)+1
+# -
+
+# ## Definer en vektor med fire tall. Hva skjer hvis du prøver å legge sammen vektoren med fire tall med en av de vektorene med tre tall?
+
+vektor_tall_4 <- 1:4
+dd
+# vektor_tall_4+vektor_tall_2
+
+# ## Kombiner de to vektorene til én vektor
+
+c(vektor_tall_2, vektor_tall_4)
+
+# ## Opprett vektoren `statsminister` med koden nedenfor. Bruk deretter funksjonene `class()`, `length()`, `unique()` og `table()` på dette objektet. Hva forteller disse? 
+
+# +
+statsminister <- c("Brundtland", "Willoch", "Brundtland", "Syse", "Brundtland", "Jagland", "Bondevik", "Stoltenberg", "Bondevik", "Stoltenberg", "Solberg", "Støre")
+
+class(statsminister)
+length(statsminister)
+unique(statsminister)
+table(statsminister)
+# -
+
+# ## Hva forteller denne? `length(unique())` 
+
+length(unique(statsminister))
+
+# ## Definer en vektor som heter `vektor` med seks tall du velger. Hva gjør koden `vektor > 5`? 
+
+vektor <- 1:6
+vektor_sjekk <- vektor > 5
+vektor_sjekk
+
+# ## Ofte kan det være lurt å gjøre om boolske verdier til tall. F.eks. `1*(vektor_sjekk)`. Hva forteller `sum(1*vektor_sjekk)` deg?
+
+1*(vektor_sjekk)
+sum(1*(vektor_sjekk))
+
+# ## Sjekk om Vedum har vært statsminister ved å bruke `%in%` 
+
+"Vedum" %in% vektor_sjekk
+
+# ## Tell antall statsministerperioder Gro Harlem Brundtland hadde ved `sum(1*...)`
+
+sum(1*(statsminister == "Brundtland"))
+
+# ## Sett sammen vektorene `parti`, `kvinne` og `statsminister` til en data.frame og gi denne et passende navn.
+
+parti <- c("AP", "H", "Ap", "H", "Ap", "Ap", "Krf", "Ap", "KrF", "Ap", "H", "Ap")
+kvinne <- c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE)
+
+statsminister_kjonn <- data.frame(parti, kvinne, statsminister)
+
+# ## Print ut, sjekk hvordan ser ut (kan bruke `View()` også). Hvordan ser du hva slags objekttype de ulike kolonnene er? 
+
+statsminister_kjonn
+
+# ## Hent ut kolonnene fra data framen ved hjelp av `$`. Bruk funksjonene `nrow()` og `ncol()` for å telle antall rader og kolonner i datasettet.
+
+# +
+statsminister_kjonn$parti
+statsminister_kjonn$kvinne
+statsminister_kjonn$statsminister
+
+nrow(statsminister_kjonn)
+ncol(statsminister_kjonn)
---
+++ løsninger/Oppgaver 3 - løsning.R
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+# # Oppgaver 3
+
+# ## Hent inn pakken `tidyverse` som skal brukes i oppgavene med funksjonen `library()`
+
+library(tidyverse)
+
+# ## Last inn filen "befolkning_per_kommune.parquet" som ligger i mappen `data` kursmateriellet. Kall objektet for `befolkning_per_kommune`.
+
+befolkning_per_kommune <- arrow::read_parquet("../data/befolkning_per_kommune.parquet")
+
+# ## Print de 6 første radene i `befolkning_per_kommune`
+
+head(befolkning_per_kommune)
+
+# ## Hvilke variabeltyper har kolonnene i filen?
+
+summary(befolkning_per_kommune)
+pillar::glimpse(befolkning_per_kommune)
+
+# ## Hva er gjennomsnittet og medianen til kolonnen value?
+
+# glimpse(befolkning_per_kommune)
+summary(befolkning_per_kommune)
+
+# ## Bruk funksjonen `rename()`til å endre navnet på kolonnene `Region` til `kommunenummer` og `value` til `personer`.
+
+befolkning_per_kommune <- befolkning_per_kommune %>%
+  rename(kommunenummer = Region, 
+         personer = value)
+
+# ## Bruk funksjonen `select()` til å fjerne variabelen `Tid` og endre rekkefølgen på kolonnene slik: `kommunenummer`, `kommunenavn`, `personer`.
+
+# +
+befolkning_per_kommune <- befolkning_per_kommune %>%
+  select(kommunenummer, kommunenavn, personer)
+
+befolkning_per_kommune %>%
+  head()
+# -
+
+# ## Hvor mange personer bor det i Bergen kommune?
+
+befolkning_per_kommune %>%
+  filter(kommunenavn == "Bergen")
+
+# ## Bruk funksjonen `filter()` for å beholde kommuner med befolkning på lavere enn eller lik 1000. Hvor mange kommnuner blir det?
+
+befolkning_per_kommune %>%
+  filter(personer <= 1000)
+
+# ## Hvilke to kommuner har flest og færrest antall innbyggere? Hvor mange innbyggere har disse kommunene?
+
+befolkning_per_kommune %>%
+  filter(personer == min(personer) | 
+           personer == max(personer))
+
+# ## Bruk funksjonen `mutate()` til å opprette en ny variabel som heter `fylkesnummer` som inneholder fylkesnummeret til hver kommune. Hint: fylkesnummeret er de to første sifrene i kommunenummeret.
+
+# +
+befolkning_per_kommune <- befolkning_per_kommune %>%
+  mutate(fylkesnummer = substr(kommunenummer, 1, 2))
+
+befolkning_per_kommune %>%
+  head()
+# -
+
+# ## Bruk funksjonene `group_by()` og `summarise()` for å beregne summen og gjennomsnittet av befolkning per fylke. Lagre resultatene i et nye objekter som heter `befolkning_per_fylke_sum` og `befolkning_per_fylke_gjennomsnitt` og kall de nye variablene for `sum` og `gjennomsnitt`
+
+# +
+befolkning_per_fylke_sum <- befolkning_per_kommune %>%
+  group_by(fylkesnummer) %>%
+  summarise(sum = sum(personer))
+
+befolkning_per_fylke_gjennomsnitt <- befolkning_per_kommune %>%
+  group_by(fylkesnummer) %>%
+  summarise(gjennomsnitt = mean(personer))
+# -
+
+# ## Koble sammen datasettene `befolkning_per_fylke_sum` og `befolkning_per_fylke_gjennomsnitt` og kall det nye objektet for `befolkning_per_fylke_2`
+
+# +
+befolkning_per_fylke_2 <- befolkning_per_fylke_sum %>%
+  left_join(befolkning_per_fylke_gjennomsnitt, by = "fylkesnummer")
+
+head(befolkning_per_fylke_2)
+# -
+
+# ## Last inn filen `"../data/fylkesinndeling.csv"` med funksjonen `read.csv2()` og print ut den første raden og kolonnenavnene i filen (filen er lagret med encoding "latin1"). Gjør deretter det samme, men legg til argumentet `header = FALSE` i funksjonen `read.csv2()`. Hva blir forskjellen?
+
+# +
+fylkesinndeling <- read.csv2("../data/fylkesinndeling.csv", sep = ";", header = FALSE, encoding = "latin1")
+fylkesinndeling[1,]
+colnames(fylkesinndeling)
+
+fylkesinndeling <- read.csv2("../data/fylkesinndeling.csv", sep = ";", header = TRUE, encoding = "latin1")
+fylkesinndeling[1,]
+colnames(fylkesinndeling)
+# -
+
+# ## Last inn filen "../data/fylkesinndeling.csv" og kall objektet `fylkesinndeling`. Endre navn på kolonnen `V1` til `fylkesnummer` og legg til ledende null med funksjonen `str_pad()`
+
+# +
+fylkesinndeling <- read.csv("../data/fylkesinndeling.csv", sep = ";", header = FALSE) %>%
+  rename(fylkesnummer = V1, 
+         fylkesnavn = V2) %>%
+  mutate(fylkesnummer = stringr::str_pad(fylkesnummer, width = 2, "left", pad = "0"))
+
+fylkesinndeling
+# -
+
+# ## Legg til navn på fylke ved å koble sammen `befolkning_per_fylke_2` og `fylkesinndeling` med funksjonen `full_join()`. Kall det nye objektet `befolkning_per_fylke_3`
+
+# +
+befolkning_per_fylke_3 <- befolkning_per_fylke_2 %>%
+  full_join(fylkesinndeling, by = "fylkesnummer")
+
+befolkning_per_fylke_3
+# -
+
+# ## Fjern rader med missing (`NA`) på variabelen `sum`.
+
+befolkning_per_fylke_3 <- befolkning_per_fylke_3 %>%
+  filter(!is.na(sum))
+
+# ## Restrukturer datasettet `befolkning_per_fylke_3` fra "bredt" til "langt" format. Hint: kolonnene `sum` og `gjennomsnitt` skal slås sammen til én kolonne. 
+
+# +
+befolkning_per_fylke_3_long <- befolkning_per_fylke_3 %>%
+  pivot_longer(cols = c("sum", "gjennomsnitt"), 
+               names_to = "variabel",
+               values_to = "personer")
+
+nrow(befolkning_per_fylke_3)
+nrow(befolkning_per_fylke_3_long)
+# -
+
+# ## Last inn filen `befolkning` fra statistikkbanken ved å kjøre kodesnutten nedenfor og beregn befolkning (totalt) for hele landet, per kommune og per fylke i tre separate objekter. Behold kun aktive fylker og kommuner (dvs. som har innbyggere i 2024)
+#
+# Hint
+# + Region: 0 = hele landet, to siffer = fylke, fire siffer = kommune
+# + Kjonn: 1 = menn, 2 = kvinner
+
+befolkning <- PxWebApiData::ApiData(07459, 
+                                    ContentsCode = T, 
+                                    Region = T,
+                                    Kjonn = T,     
+                                    Alder = T,                 
+                                    Tid = "2024")[[2]]
+
+# +
+befolkning_hele_landet <- befolkning %>%
+  filter(Region == 0) %>%
+  group_by(Region) %>%
+  summarise(value = sum(value))
+
+befolkning_fylke <- befolkning %>%
+  filter(nchar(Region) == 2) %>%
+  group_by(Region) %>%
+  summarise(value = sum(value)) %>%
+  filter(value != 0)
+
+befolkning_kommune <- befolkning %>%
+  filter(nchar(Region) == 4) %>%
+  group_by(Region) %>%
+  summarise(value = sum(value)) %>%
+  filter(value != 0)
+# -
+
+# ## Bruk `befolkning` fra forrige oppgave og opprett en ny variabel som heter `aldersgruppe` der alder er gruppert etter følgende inndeling: "0-15", "16-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75+"
+#
+# Beregn deretter befolkning per aldersgruppe for hele landet, per kommune og per fylke i tre separate objekter.
+
+# +
+befolkning_2 <- befolkning %>%
+  mutate(Alder = gsub("\\+", "", Alder), 
+         Alder_num = as.numeric(Alder), 
+         aldersgruppe = case_when(Alder_num %in% 0:15 ~ "0-15", 
+                                  Alder_num %in% 16:24 ~ "16-24", 
+                                  Alder_num %in% 25:34 ~ "25-34", 
+                                  Alder_num %in% 35:44 ~ "35-44", 
+                                  Alder_num %in% 45:54 ~ "45-54", 
+                                  Alder_num %in% 55:64 ~ "55-64",
+                                  Alder_num %in% 65:74 ~ "65-74",
+                                  Alder_num >= 75 ~ "75 og eldre",
+                                  TRUE ~ ""))
+
+befolkning_2_hele_landet <- befolkning_2 %>%
+  filter(Region == 0) %>%
+  group_by(Region, aldersgruppe) %>%
+  summarise(value = sum(value))
+
+befolkning_2_fylke <- befolkning_2 %>%
+  filter(nchar(Region) == 2) %>%
+  group_by(Region, aldersgruppe) %>%
+  summarise(value = sum(value)) %>%
+  filter(value != 0)
+
+befolkning_2_kommune <- befolkning_2 %>%
+  filter(nchar(Region) == 4) %>%
+  group_by(Region, aldersgruppe) %>%
+  summarise(value = sum(value)) %>%
+  filter(value != 0)
+# -
+
+# ## Bruk funksjonen `bind_rows()` for å legge sammen de tre objektene du opprettet i forrige oppgave til ett nytt objekt som heter `befolking_per_aldersgrupper`.
+
+befolking_per_aldersgrupper <- bind_rows(befolkning_2_hele_landet, befolkning_2_fylke, befolkning_2_kommune)
+
+# ## Lagre objektet `befolking_per_aldersgrupper` i mappen "../data/" i .csv-format og les deretter inn filen for å kontrollere at filen ble lagret riktig. Lagre også filen i .xlsx og .parquet-format (og les inn filene etterpå)
+
+# +
+write.csv2(befolking_per_aldersgrupper, "../data/befolking_per_aldersgrupper.csv")
+
+openxlsx::write.xlsx(befolking_per_aldersgrupper, file = "../data/befolking_per_aldersgrupper.xlsx",
+                     rowNames = FALSE,
+                     showNA = FALSE,
+                     overwrite=T)
+
+arrow::write_parquet(befolking_per_aldersgrupper, "../data/befolking_per_aldersgrupper.parquet")
+# -
+
+# ## Åpen oppgave: Les inn et datasett du jobber med og bruk funksjonene `rename()`, `select()`, `filter()`, `group_by()`, `summarise()` osv. til å gjøre gjøre oppgaver du vanligvis må løse.
+
+
+
+
